4f5839274f2bf11739c3c4367513241ffcb5069a
cocoa <jwv0323@naver.com>
2023-03-30 21:30:41 +0900

    feat: Title component

---
+++ src/pages/Detail/Detail.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/order */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import './Detail.scss';
+import Title from '../../components/Title/Title';
+
+function Detail() {
+  return (
+    <div className="Detail">
+      <Title />
+    </div>
+  );
+}
+
+export default Detail;
