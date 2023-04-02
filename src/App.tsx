3633dc51a6d1bebebdd3bf1ebdf6dba847822d6f
cocoa <jwv0323@naver.com>
2023-04-03 00:26:04 +0900

    feat: IconInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Detail from './pages/Detail/Detail';
 
 function App() {
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,13 +1,13 @@
-/* eslint-disable import/order */
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './Detail.scss';
 import Title from '../../components/Title/Title';
+import IconInfo from '../../components/IconInfo/IconInfo';
 
 function Detail() {
   return (
     <div className="Detail">
       <Title />
+      <IconInfo />
     </div>
   );
 }
